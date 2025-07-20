7185fdd63ac27f9414f1f6a0c8d43cd15b3b0da3
Jnanashish <jnanashish@Snapmints-Laptop.local>
2025-07-20 13:27:19 +0530

    analytics added

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Analytics } from "@vercel/analytics/react"
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <App />
+  <>
+    <App />
+    <Analytics />
+  </>
+
+
 );
